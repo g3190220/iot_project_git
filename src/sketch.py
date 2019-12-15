0ec9d6262e9b8f0b2f240123e15c9dc02b64e292
g3190220 <g3190220@gmail.com>
2019-12-15 20:17:22 +0800

    Add all

--- src/sketch.py
+++ src/sketch.py
@@ -4,15 +4,19 @@
 @file: sketch.py
 @time: 2018/7/11 11:22
 """
-from flask import Flask, render_template,request,jsonify
+from flask import Flask,flash, render_template,request,jsonify
 from werkzeug.datastructures import CombinedMultiDict, MultiDict
 import datetime
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)      
 
 @app.route('/SmartNote/', methods=['GET','POST'])
 
 def Hello():
+    return render_template('SmartNote.html')
+@app.route('/Go_back/')
+def Go_back():
     return render_template('SmartNote.html')
 
 @app.route('/state_submit/', methods=['GET', 'POST'])
@@ -32,21 +36,33 @@ def note_submit():
 
 @app.route('/control_led/', methods=['GET', 'POST'])  
 def control_led():
-    if request.method == 'POST': 
-        now = datetime.datetime.now()
-        now_c=now.strftime("%-I:%M%P")
+    # blinking function
+    #def blink(pin):
+        #GPIO.output(pin,GPIO.HIGH)
+        #ime.sleep(1)
+        #GPIO.output(pin,GPIO.LOW)
+        #time.sleep(1)
+        #return
+    if request.method == 'POST':
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(12, GPIO.OUT)
         time_=request.form.get("time_get")
-        print(time_)
-        pirnt(now_c)
-        # if time_== now_c
-        #     print("time's up!")
-    return render_template('SmartNote.html')
-    
-   
-    
-    
+        flag=1
+        print('You turn on your pi alarm!')
+        while flag:
+           now = datetime.datetime.now()
+           now_c=now.strftime("%-I:%M%P")
+           if time_== now_c:
+                print("time's up!")
+                flag=0
+                return render_template('SmartNote.html')         
+        #print('It is %s'%(now_c))
+        #print('our alarm is %s'%(time_))
+        #return render_template('SmartNote.html')
+           
 
 
 if __name__ == '__main__':
+    app.secret_key='12345'
     app.debug=True
     app.run(host='0.0.0.0',port=5000)    # 執行我們的伺服器！
