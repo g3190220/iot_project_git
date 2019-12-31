5bd87b936928369778668e3b166a35cd18feaf8e
g3190220 <g3190220@gmail.com>
2019-12-31 14:50:29 +0800

    add all

---
+++ src/epd_empty.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.getcwd(), 'pic')
+libdir = os.path.join(os.getcwd(), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+import logging
+from waveshare_epd import epd2in9
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+import datetime
+import json
+
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in9 Demo")
+    epd = epd2in9.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.lut_full_update)
+    epd.Clear(0xFF)
+            
+        
+    epd.init(epd.lut_partial_update)    
+    epd.Clear(0xFF)
+   
