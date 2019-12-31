64f6e6d657bd236be061f63ebd1133ff99f3c4f2
g3190220 <g3190220@gmail.com>
2019-12-31 14:55:25 +0800

    add all

--- src/epd_empty.py
+++ src/epd_empty.py
@@ -29,11 +29,11 @@ logging.info("init and Clear")
 epd.init(epd.lut_full_update)
 epd.Clear(0xFF)
 
-wakeup_image = Image.new('1', (epd.height, epd.width), 255)
-wakeup_draw = ImageDraw.Draw(wakeup_image)
-wakeup_draw.text((15, 15),'Times up!!', font = font18, fill = 0)
-wakeup_draw.line([(0, 12), (epd.width,12)],fill = 0, width = 3)
-epd.display(epd.getbuffer(wakeup_image))
+#wakeup_image = Image.new('1', (epd.height, epd.width), 255)
+#wakeup_draw = ImageDraw.Draw(wakeup_image)
+#wakeup_draw.text((30, 30),'Times up!!', font = font18, fill = 0)
+#wakeup_draw.line([(0, 12), (epd.width,12)],fill = 0, width = 3)
+#epd.display(epd.getbuffer(wakeup_image))
             
     
    
