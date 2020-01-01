e1ec09bf2fb2f1e2dad819449463f81ddab36012
g3190220 <g3190220@gmail.com>
2020-01-01 22:44:04 +0800

    add all

--- src/touch.py
+++ src/touch.py
@@ -24,13 +24,13 @@ def setup():
 def loop():
 	while True:
 		if GPIO.input(TouchPin) == GPIO.LOW:
-			epd = epd2in9.EPD()
+			#epd = epd2in9.EPD()
 			print("wait to touch...")
-			wait_image = Image.new('1', (epd.height, epd.width), 255)
-        	wait_draw = ImageDraw.Draw(wait_image)
-        	wait_draw.rectangle((10, 10, 100, 40), fill = 0)
-        	wait_draw.text((15, 15), "do something...", font = font18, fill = 255)
-        	epd.display(epd.getbuffer(wait_image))	
+			#wait_image = Image.new('1', (epd.height, epd.width), 255)
+        	#wait_draw = ImageDraw.Draw(wait_image)
+        	#wait_draw.rectangle((10, 10, 100, 40), fill = 0)
+        	#wait_draw.text((15, 15), "do something...", font = font18, fill = 255)
+        	#epd.display(epd.getbuffer(wait_image))	
 		else:
 			print ('you touched!')
 			epd_2in9.getDHTdata()
